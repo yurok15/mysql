55cbff18f46789a068b308d5841543cb25479b83
Yuriy Zhigulskiy <yuriy.zhigulskiy@acquire.io>
2022-10-21 14:05:40 +0300

    init

--- init.sh
+++ init.sh
@@ -4,11 +4,11 @@ MASTER_PWD="Hui2oe7thaeshin1oohai5bi9"
 SLAVE_PWD="aeveekof1BahDa9eichoo3phe"
 CREATE_REPLICATION_USER='CREATE USER "replicator"@"%" IDENTIFIED BY "feipuphePheekahtuDo1Guig6"; GRANT REPLICATION SLAVE ON *.* TO "replicator"@"%"; FLUSH PRIVILEGES;'
 
-docker-compose down -v
+/home/ubuntu/.docker/cli-plugins/docker-compose down -v
 rm -rf ./master/data/*
 rm -rf ./slave/data/*
-docker-compose build
-docker-compose up -d
+/home/ubuntu/.docker/cli-plugins/docker-compose build
+/home/ubuntu/.docker/cli-plugins/docker-compose up -d
 
 until docker exec master bash -c 'export MYSQL_PWD='"$MASTER_PWD"'; mysql -uroot -h 127.0.0.1 -e ";"'
 do
@@ -18,7 +18,7 @@ done
 docker exec master sh -c "export MYSQL_PWD=$MASTER_PWD; mysql -u root -e '$CREATE_REPLICATION_USER'"
 
 
-until docker-compose exec slave sh -c 'export MYSQL_PWD='"$SLAVE_PWD"'; mysql -u root -e ";"'
+until /home/ubuntu/.docker/cli-plugins/docker-compose exec slave sh -c 'export MYSQL_PWD='"$SLAVE_PWD"'; mysql -u root -e ";"'
 do
     echo "Waiting for slave database connection..."
     sleep 5
