87c76a1dd8e172a8bd6f03fee39bf3da487aede4
Yuriy Zhigulskiy <yuriy.zhigulskiy@acquire.io>
2022-10-21 15:03:20 +0300

    init

--- migrate/schema/3_create_sbtest_schema.down.sql
+++
@@ -1,2 +0,0 @@
-DROP user sbtest;
-DROP database sbtest;
--- migrate/schema/3_create_sbtest_schema.up.sql
+++
@@ -1,3 +0,0 @@
-CREATE SCHEMA sbtest;
-CREATE USER sbtest@'%' IDENTIFIED BY 'eiciejei2aichuta7pooLeifi';
-GRANT ALL PRIVILEGES ON sbtest.* to sbtest@'%';
